498b62cb4121efeda158911c5ff9eb97b1557c0f
@NicollMitchManingat <nicollmitch@gmail.com>
2025-11-15 11:59:38 +0800

    Add CRUD operations for books and students; configure CORS and logging middleware

--- app.js
+++ app.js
@@ -2,10 +2,25 @@ import express from 'express';
 import 'dotenv/config.js';
 import bookRoutes from './router/BookRoutes.js';
 import studentRoutes from './router/StudentRoutes.js';
+import cors from "cors";
 
+//init app
 const app = express();
-app.use(express.json());
 
+//enable cors to frontend
+let corsOptions = {
+    origin: process.env.ORIGIN
+}
+
+//middleware
+app.use(express.json());
+app.use(cors(corsOptions));
+
+//this is used to log the request on the console
+app.use((req, res, next) => {
+    console.log(req.path, req.method);
+    next();
+});
 
 try {
     app.listen(process.env.PORT || 5000, () => {
--- controllers/StudentController.js
+++ controllers/StudentController.js
@@ -13,3 +13,37 @@ export const fetchStudents = async (req, res) => {
         })
     }
 }
+
+export const createStudent = async (req, res) => {
+    const {name , srcode , course} = req.body;
+    try {
+        const studentId = await StudentModel.inserStudent(name, srcode, course);
+        res.status(200).json({success: true, data: studentId});
+    }catch (e) {
+        console.log(e)
+        res.status(500).json({success: false, message: "Internal Server Error"})
+    }
+}
+
+export const editStudent = async (req, res) => {
+    const {name, srcode, course} = req.body;
+    const {studentId} = req.params;
+    try {
+        const updateId = await StudentModel.updateStudent(name, srcode, course, studentId);
+        res.status(200).json({success: true, data: updateId});
+    }catch (e) {
+        console.log(e)
+        res.status(500).json({success: false, message: "Internal Server Error"})
+    }
+}
+
+export const deleteStudent = async (req, res) => {
+    const {studentId} = req.params;
+    try {
+        const deletedId = await StudentModel.deleteStudent(studentId);
+        res.status(200).json({success: true, data: deletedId});
+    }catch (e) {
+        console.log(e)
+        res.status(500).json({success: false, message: "Internal Server Error"})
+    }
+}
